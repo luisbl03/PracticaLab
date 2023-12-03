75c0aa5c51f38a97e90f9a66159c3a9d7727d43c
javicuarteroUCLM <Javier.Cuartero@alu.uclm.es>
2023-12-03 21:37:44 +0100

    Prueba gestor

--- PracticaLab/Gestor.xaml.cs
+++ PracticaLab/Gestor.xaml.cs
@@ -1,40 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Xml;
 
-namespace PracticaLab
-{
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
-    public partial class Gestor : Window
+    namespace PracticaLab
     {
-        public Gestor()
+        public partial class Gestor : Window
         {
-            InitializeComponent();
+            public List<Paciente> Pacientes { get; set; }
 
-            // Crear una lista de 25 elementos con el formato "paciente n"
-            List<string> pacientes = new List<string>();
-            for (int i = 1; i <= 25; i++)
+            public Gestor()
             {
-                pacientes.Add($"Paciente {i}");
+                InitializeComponent();
+
+                Pacientes = new List<Paciente>();
+
+                try
+                {
+                    // Ruta al archivo pacientes.xml
+                    string rutaArchivo = "C:\\Users\\jcc20\\source\\repos\\luideoz\\PracticaLab\\PracticaLab\\Datos\\pacientes.xml"; // Poned vuestra ruta
+
+                    // Crear un objeto XmlDocument
+                    XmlDocument xmlDoc = new XmlDocument();
+
+                    // Cargar el documento XML desde el archivo
+                    xmlDoc.Load(rutaArchivo);
+
+                    // Obtener la lista de nodos de pacientes
+                    XmlNodeList pacientesXml = xmlDoc.SelectNodes("/Pacientes/Paciente");
+
+                    // Iterar a través de los pacientes y agregar a la lista de Pacientes
+                    foreach (XmlNode pacienteXml in pacientesXml)
+                    {
+                        Paciente paciente = new Paciente
+                        {
+                            Nombre = pacienteXml.SelectSingleNode("Nombre").InnerText,
+                            Apellido1 = pacienteXml.SelectSingleNode("Apellido1").InnerText,
+                            Apellido2 = pacienteXml.SelectSingleNode("Apellido2").InnerText,
+                            DNI = pacienteXml.SelectSingleNode("DNI").InnerText,
+                            Telefono = pacienteXml.SelectSingleNode("Telefono").InnerText,
+                            Direccion = pacienteXml.SelectSingleNode("Direccion").InnerText
+                        };
+
+                        Pacientes.Add(paciente);
+                    }
+
+                    // Asignar la lista de pacientes como origen de datos para el ListBox
+                    Lista_de_pacientes.ItemsSource = Pacientes;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al leer el archivo XML: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
-
-            // Asignar la lista como origen de datos para el ListBox
-            Lista_de_pacientes.ItemsSource = pacientes;
         }
-
     }
-
-}
---
+++ PracticaLab/Paciente.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticaLab
+{
+    public class Paciente
+    {
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string DNI { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido1} {Apellido2}";
+        }
+    }
+}
