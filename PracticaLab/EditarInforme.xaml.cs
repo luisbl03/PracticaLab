49a487b40c804b327f56b115cf4edec070e6be11
javicuarteroUCLM <146332399+javicuarteroUCLM@users.noreply.github.com>
2023-12-26 11:56:12 +0100

    Mejoras en los textBox
    Borrrado automático del texto predeterminado al escribir en los textBox

--- PracticaLab/EditarInforme.xaml.cs
+++ PracticaLab/EditarInforme.xaml.cs
@@ -22,6 +22,39 @@ namespace PracticaLab
         public EditarInforme()
         {
             InitializeComponent();
+
+            //Asignamos los eventos a los TextBox
+            AsignarEventosTextBox(txtPatologiasPrevias, "Insertar patologías previas");
+            AsignarEventosTextBox(txtDolencias, "Insertar dolencias");
+            AsignarEventosTextBox(txtTratamiento, "Insertar tratamiento");
+        }
+
+        private void AsignarEventosTextBox(TextBox textBox, string textoPredeterminado)
+        {
+            // Configura el color gris claro cuando el TextBox no tiene foco
+            textBox.Foreground = Brushes.LightGray;
+            textBox.Text = textoPredeterminado;
+
+            // Evento GotFocus: se dispara cuando el TextBox recibe el foco
+            textBox.GotFocus += (sender, e) =>
+            {
+                if (textBox.Text == textoPredeterminado)
+                {
+                    textBox.Text = string.Empty; // Borra el texto predeterminado
+                    textBox.Foreground = Brushes.Black; // Cambia el color del texto
+                }
+            };
+
+            // Evento LostFocus: se dispara cuando el TextBox pierde el foco
+            textBox.LostFocus += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    // Restaura el texto predeterminado y el color cuando el cuadro de texto está vacío
+                    textBox.Text = textoPredeterminado;
+                    textBox.Foreground = Brushes.LightGray;
+                }
+            };
         }
     }
 }
