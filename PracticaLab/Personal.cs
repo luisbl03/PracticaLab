ff44ae814278fad58b8c882fcfd77ab6df25a7fe
alberto-is <albertobarrais12@gmail.com>
2023-12-27 16:35:18 +0100

    clase personal creada

---
+++ PracticaLab/Personal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaLab
+{
+    internal class Personal
+    {
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public long numTelefono { get; set; }
+        public string correo { get; set; }
+        public int edad { get; set; }
+        public string trabajo { get; set; }
+        public Personal(string nombre, string apellidos, long telef, string correo, int edad, string trabajo)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            numTelefono = telef;
+            this.correo = correo;
+            this.edad = edad;
+            this.trabajo = trabajo;
+        }
+
+    }
+}
