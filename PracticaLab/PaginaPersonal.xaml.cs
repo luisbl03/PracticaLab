bbfeb1aedb9ccadfa5a2079919c19d4419cc2ef3
alberto-is <albertobarrais12@gmail.com>
2023-12-27 23:14:09 +0100

    Avance en PaginaPersonal

--- PracticaLab/PaginaPersonal.xaml.cs
+++ PracticaLab/PaginaPersonal.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -22,62 +23,52 @@ namespace PracticaLab
     /// </summary>
     public partial class Page1 : Page
     {
+        public ObservableCollection<Nomina> Nominas { get; set; }
+
         public Page1()
         {
             InitializeComponent();
             // Crear un objeto XmlDocument
             XmlDocument xmlDocPersonal = new XmlDocument();
 
-        //almacenamos la informacion de "pacientes.xml" en la variable fichero
-       // var fichero = Application.GetResourceStream(new Uri(" \\Datos\\personal.xml", UriKind.Relative));
+            //almacenamos la informacion de "pacientes.xml" en la variable fichero
+            // var fichero = Application.GetResourceStream(new Uri(" \\Datos\\personal.xml", UriKind.Relative));
 
             // Cargar el documento XML desde el archivo
-           // xmlDocPersonal.Load(fichero.Stream);
+            // xmlDocPersonal.Load(fichero.Stream);
 
             // Obtener la lista de nodos del personale
-           // XmlNodeList personal = xmlDocPersonal.SelectNodes("/Personal/Personal");
-
+            // XmlNodeList personal = xmlDocPersonal.SelectNodes("/Personal/Personal");
+            Nominas = new ObservableCollection<Nomina>();
+            datagridNominas.ItemsSource = Nominas;
         }
-        private bool modo1 = true;
-        private void bnEdicion_Click1(object sender, RoutedEventArgs e)
+        private void AgregarNomina_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            if (modo1 == true)
-            {
-                txtNombre.IsReadOnly = false;
-                txtApellido1.IsReadOnly = false;
-                txtApellido2.IsReadOnly = false;
-                txtDNI.IsReadOnly = false;
-                txtTelefono.IsReadOnly = false;
-                txtDireccion.IsReadOnly = false;
-                btn.Content = "Guardar";
-            }
-            else
-            {
-                txtNombre.IsReadOnly = true;
-                txtApellido1.IsReadOnly = true;
-                txtApellido2.IsReadOnly = true;
-                txtDNI.IsReadOnly = true;
-                txtTelefono.IsReadOnly = true;
-                txtDireccion.IsReadOnly = true;
-                btn.Content = "Editar";
-                /*
-                // Guardar los cambios en el paciente (no se guarda en la persistencia)
-                if (seleccionado == true)
-                {
-                    Paciente pacienteSeleccionado = (Paciente)Lista_de_pacientes.SelectedItem;
-                    pacienteSeleccionado.Nombre = txtNombre.Text;
-                    pacienteSeleccionado.Apellido1 = txtApellido1.Text;
-                    pacienteSeleccionado.Apellido2 = txtApellido2.Text;
-                    pacienteSeleccionado.DNI = txtDNI.Text;
-                    pacienteSeleccionado.Telefono = txtTelefono.Text;
-                    pacienteSeleccionado.Direccion = txtDireccion.Text;
-                }
-                */
-            }
-            modo1 = !modo1;
+
+            // Ejemplo de cómo agregar una nómina ficticia a la lista
+            // Inicializar la lista de nóminas
+            Nominas.Add(new Nomina { Fecha = DateTime.Now, Monto = 2500.0 });
 
         }
-       
+        private void EliminarNomina_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtener las nóminas seleccionadas en el DataGrid
+            var nominasSeleccionadas = datagridNominas.SelectedItems.Cast<Nomina>().ToList();
+
+            // Eliminar las nóminas seleccionadas de la colección
+            foreach (var nominaSeleccionada in nominasSeleccionadas)
+            {
+                Nominas.Remove(nominaSeleccionada);
+            }
+        }
+
     }
+    public class Nomina
+        {
+            public DateTime Fecha { get; set; }
+            public double Monto { get; set; }
+        }
+
+       
+  
 }
