a3b69811c64bad721e14ae923e11bf7051476954
javicuarteroUCLM <Javier.Cuartero@alu.uclm.es>
2024-01-10 12:16:04 +0100

    Mejora de comentarios en Informe

--- PracticaLab/Informe.cs
+++ PracticaLab/Informe.cs
@@ -7,25 +7,25 @@ namespace PracticaLab
         public string Descripcion { get; set; }
         public DateTime FechaInforme { get; set; }
 
-        // Nueva propiedad para indicar si el informe se ha guardado
+        //Popiedad para indicar si el informe se ha guardado
         public bool Guardado { get; set; }
 
-        // Nueva propiedad para contener la descripción del informe antes de cualquier modificación
+        //Propiedad para contener la descripción del informe antes de cualquier modificación
         public string DescripcionInforme { get; set; }
 
         public Informe()
         {
-            Guardado = true; // Inicialmente, el informe se considera guardado
+            Guardado = true; // Inicialmente el informe se considera guardado
         }
 
         public void ActualizarDescripcionInicial()
         {
-            DescripcionInforme = Descripcion; // Guardar la descripción inicial al cargar el informe
+            DescripcionInforme = Descripcion; // Guarda la descripción inicial al cargar el informe
         }
 
         public void MarcarComoGuardado()
         {
-            Guardado = true; // Marcar el informe como guardado después de actualizar la descripción
+            Guardado = true; // Marca el informe como guardado después de actualizar la descripción
         }
 
         public override string ToString()
